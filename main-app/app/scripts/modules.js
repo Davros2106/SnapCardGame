01eed881f349577dc76d1b2141f713f63368db59
Davros2106 <david.robson@tombola-3.local>
2015-05-26 15:54:16 +0100

    Game runs in console without players.

--- main-app/app/scripts/modules.js
+++ main-app/app/scripts/modules.js
@@ -1,7 +1,1 @@
-angular.module('tombola.snapCardGame.providers.gameApiProxy',[]);
-angular.module('tombola.snapCardGame.providers.gameModel',[]);
-angular.module('tombola.snapCardGame.directives',[]);
-angular.module('tombola.snapCardGame', [
-    'tombola.snapCardGame.providers.gameApiProxy',
-    'tombola.snapCardGame.providers.gameModel',
-    'tombola.snapCardGame.directives']);
+angular.module('tombola.snapCardGame',[]);
--- main-app/app/scripts/providers/game-model.js
+++ main-app/app/scripts/providers/game-model.js
@@ -1,9 +1,32 @@
-/**
- @module Tombola.Core.<MODULE_NAME>
- @class <ENTITY_NAME>
- */
 (function () {
     'use strict';
+    angular.module('tombola.snapCardGame');
 
-    // FEED ME CODE....
+    var hand1 = [1,2,3,4,5];
+
+    var hand2 = [1,2,3,4,5];
+
+    var deck = [hand1,hand2];
+
+    hand1.sort(function(){
+        return (Math.round(Math.random())-0.5);
+    });
+
+    hand2.sort(function(){
+        return (Math.round(Math.random())-0.5);
+    });
+
+    for (var i = 0; i < hand1.length; i++) {
+        console.log(hand1[i]);
+
+       console.log(hand2[i]);
+
+
+        if (hand1[i] == hand2[i]) {
+            console.log('snap');
+        }
+    }
+
+    console.log(deck);
+
 })();
