1e7671380de58919bb7c18adcd0210e744ba3504
Davros2106 <david.robson@tombola-3.local>
2015-05-27 13:18:08 +0100

    Deck now being generated

--- main-app/app/scripts/providers/game-model.js
+++ main-app/app/scripts/providers/game-model.js
@@ -2,31 +2,60 @@
     'use strict';
     angular.module('tombola.snapCardGame');
 
-    var hand1 = [1,2,3,4,5];
+    var deck = [];
 
-    var hand2 = [1,2,3,4,5];
+    var suits = ['Trebuchet', 'Rare Pepe', 'Neckbeard', 'Shrek'];
 
-    var deck = [hand1,hand2];
+    var Card = function (number, suit) {
 
-    hand1.sort(function(){
-        return (Math.round(Math.random())-0.5);
-    });
+        this.suit = suit;
 
-    hand2.sort(function(){
-        return (Math.round(Math.random())-0.5);
-    });
-
-    for (var i = 0; i < hand1.length; i++) {
-        console.log(hand1[i]);
-
-       console.log(hand2[i]);
+    };
 
 
-        if (hand1[i] == hand2[i]) {
-            console.log('snap');
+    var makeDeck = function() {
+
+        for (var i = 0; i < suits.length; i++) {
+            for (var j = 0; j < 5; j++) {
+                deck[i*5+ j] = new Card(j, suits[i]);
+            }
         }
-    }
 
-    console.log(deck);
+    };
+
+makeDeck();
+
+console.log(deck);
+
+
 
 })();
+
+//    var hand1 = [1,2,3,4,5];
+//
+//    var hand2 = [1,2,3,4,5];
+//
+//    var deck = [hand1,hand2];
+//
+//    hand1.sort(function(){
+//        return (Math.round(Math.random())-0.5);
+//    });
+//
+//    hand2.sort(function(){
+//        return (Math.round(Math.random())-0.5);
+//    });
+//
+//    for (var i = 0; i < hand1.length; i++) {
+//        console.log(hand1[i]);
+//
+//       console.log(hand2[i]);
+//
+//
+//        if (hand1[i] == hand2[i]) {
+//            console.log('snap');
+//        }
+//    }
+//
+//    console.log(deck);
+//
+//})();
