e55c62891553053b523c7cddfbd0064b974ace83
Davros2106 <david.robson@tombola-3.local>
2015-06-01 09:44:39 +0100

    Automatic game of snap is working in the console with no players

--- main-app/app/scripts/providers/game-model.js
+++ main-app/app/scripts/providers/game-model.js
@@ -2,60 +2,49 @@
     'use strict';
     angular.module('tombola.snapCardGame');
 
+
     var deck = [];
 
+
     var suits = ['Trebuchet', 'Rare Pepe', 'Neckbeard', 'Shrek'];
-
-    var Card = function (number, suit) {
-
-        this.suit = suit;
-
-    };
-
 
     var makeDeck = function() {
 
         for (var i = 0; i < suits.length; i++) {
             for (var j = 0; j < 5; j++) {
-                deck[i*5+ j] = new Card(j, suits[i]);
+                deck[i*5 + j] = suits[i];
             }
         }
 
     };
 
 makeDeck();
+    deck.sort(function(){
+        return (Math.round(Math.random())-0.5);
+    });
 
-console.log(deck);
+    var hand1 = deck.splice(0,10);
+
+    var hand2 = deck.splice(0,10);
 
 
+    for (var i = 0; i < hand1.length; i++) {
+
+        console.log(hand1[i]);
+
+       console.log(hand2[i]);
+
+
+        if (hand1[i] === hand2[i]) {
+            alert('snap');
+
+        }
+        else {
+            alert('continue');
+        }
+
+    }
+
+    console.log(deck);
 
 })();
-
-//    var hand1 = [1,2,3,4,5];
-//
-//    var hand2 = [1,2,3,4,5];
-//
-//    var deck = [hand1,hand2];
-//
-//    hand1.sort(function(){
-//        return (Math.round(Math.random())-0.5);
-//    });
-//
-//    hand2.sort(function(){
-//        return (Math.round(Math.random())-0.5);
-//    });
-//
-//    for (var i = 0; i < hand1.length; i++) {
-//        console.log(hand1[i]);
-//
-//       console.log(hand2[i]);
-//
-//
-//        if (hand1[i] == hand2[i]) {
-//            console.log('snap');
-//        }
-//    }
-//
-//    console.log(deck);
-//
-//})();
